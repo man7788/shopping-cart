ec1965b209b001a9396dff29f13f7b9f603548da
man7788 <jerman1841@gmail.com>
2023-06-21 05:56:58 +0800

    Setup components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Homepage from './components/Homepage';
+import Nav from './components/Nav';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div>
+      <Homepage />
+      <Nav />
+      <Cart />
     </div>
   );
 }
 
 export default App;
+
+// RouteSwitch -> App       -> Nav
+//             -> Homepage
+//             -> Shop Page -> Card
+//             -> Cart
---
+++ src/components/ShopPage.js
@@ -0,0 +1,5 @@
+function ShopPage() {
+  return <div></div>;
+}
+
+export default ShopPage;
