9514db8b8081ab7d69c4ce23efa1c8d8a0fc84d2
man7788 <jerman1841@gmail.com>
2023-06-24 07:00:24 +0800

    Add tests for input field

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -6,11 +6,37 @@ it('should invoke onClick event', async () => {
   const user = userEvent.setup();
   const mockFn = jest.fn();
 
-  render(<Card setItem={mockFn} />);
+  render(<Card setCartItems={mockFn} />);
 
-  const target = screen.getByText('Item');
+  const target = screen.getAllByRole('button', { name: 'Add' });
 
-  await user.click(target);
+  await user.click(target[0]);
 
   expect(mockFn).toHaveBeenCalled();
+});
+
+it.only('should display number in input field', async () => {
+  const user = userEvent.setup();
+
+  render(<Card />);
+
+  const target = screen.getAllByRole('spinbutton');
+
+  await user.type(target[0], '12345');
+
+  expect(target[0].value).toMatch('12345');
+});
+
+it('should reset input field to 0', async () => {
+  const user = userEvent.setup();
+
+  render(<Card />);
+
+  const button = screen.getAllByRole('button', { name: 'Add' });
+  const input = screen.getAllByRole('spinbutton');
+
+  await user.type(input[0], '12345');
+  await user.click(button[0]);
+
+  expect(input[0].value).toMatch('0');
 });
