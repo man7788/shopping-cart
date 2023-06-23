f551bceee5636ff7bfca1378bd750733e60b1927
man7788 <jerman1841@gmail.com>
2023-06-24 06:59:53 +0800

    Add comment

--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
 
 xit('should display number passed as props', () => {
+  // BrowserRouter for rendering <Link/> tag
   render(<Nav item={'5'} />, { wrapper: BrowserRouter });
 
   const display = screen.getByTestId('cart');
