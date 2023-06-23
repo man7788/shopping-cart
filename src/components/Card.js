f1b45e8119b47ec4c988e3793b5f050a560fda26
man7788 <jerman1841@gmail.com>
2023-06-24 07:00:12 +0800

    Add input field

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,27 @@
-function Card({ item, setItem }) {
-  const addOne = () => {
-    setItem(item + 1);
+import { useState } from 'react';
+
+function Card({ cartItems, setCartItems, itemName = 'Item Name' }) {
+  const [quantity, setQuantity] = useState(0);
+
+  const addToCart = (e) => {
+    e.preventDefault();
+    setCartItems(Number(cartItems) + Number(quantity));
+    setQuantity(0);
+  };
+
+  const handleChange = (e) => {
+    setQuantity(e.target.value);
   };
 
   return (
     <div>
-      <div className="item" onClick={addOne}>
-        Item
-      </div>
+      {itemName}
+      <form onSubmit={addToCart}>
+        <input onChange={handleChange} value={quantity} type="number"></input>
+        <button data-testid="add" type="submit">
+          Add
+        </button>
+      </form>
     </div>
   );
 }
