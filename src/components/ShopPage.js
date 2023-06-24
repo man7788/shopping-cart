859767ca395c9e7afb96828beaeadc1cb32edd26
man7788 <jerman1841@gmail.com>
2023-06-25 00:38:32 +0800

    Add storage with multiple items

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -1,10 +1,24 @@
 import Card from './Card';
+import uniqid from 'uniqid';
 
 function ShopPage({ cartItems, setCartItems }) {
+  const storage = ['Alpha', 'Bravo', 'Charlie', 'Delta'];
+
+  const display = storage.map((item) => {
+    return (
+      <Card
+        cartItems={cartItems}
+        setCartItems={setCartItems}
+        itemName={item}
+        key={uniqid()}
+      />
+    );
+  });
+
   return (
     <div>
       Shop Page
-      <Card cartItems={cartItems} setCartItems={setCartItems} />
+      {display}
     </div>
   );
 }
