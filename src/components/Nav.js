a627da4686be30bfbd968561bd2e52ccf640d42f
man7788 <jerman1841@gmail.com>
2023-06-28 07:23:04 +0800

    Add nav-bar logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,30 @@
-import { Link } from 'react-router-dom';
-
+import { Link, NavLink } from 'react-router-dom';
+import '../styles/Nav.css';
 import Cart from './Cart';
+import logo from '../images/logo.svg';
 
 function Nav({ cartItems }) {
   return (
-    <div>
-      <Link to="/">Homepage</Link>
-      <br />
-      <Link to="/shop-page">Shop Page</Link>
+    <div className="nav-div">
+      <img className="logo" src={logo} alt="logo" />
+      <div className="pages">
+        <div className="page">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+          >
+            Homepage
+          </NavLink>
+        </div>
+        <div className="page">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+            to="/shop-page"
+          >
+            Shop Page
+          </NavLink>
+        </div>
+      </div>
       <Cart cartItems={cartItems} />
     </div>
   );
