32d6a7fb41f10d8d455238d3b501fac4dba0cd34
man7788 <jerman1841@gmail.com>
2023-08-12 08:31:07 +0800

    Update store item to fetch data

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -1,30 +1,52 @@
-import uniqid from 'uniqid';
+// import uniqid from 'uniqid';
 import { useOutletContext } from 'react-router-dom';
 import styles from './ShopPage.module.css';
 import Card from '../Card/Card';
+import useStoreURL from '../UseStoreURL';
 
 function ShopPage() {
   const { cartItems, setCartItems } = useOutletContext();
+  const { storeURL, error, loading } = useStoreURL();
 
-  const storage = [
-    { itemName: 'Alpha', id: uniqid() },
-    { itemName: 'Bravo', id: uniqid() },
-    { itemName: 'Charlie', id: uniqid() },
-    { itemName: 'Delta', id: uniqid() },
-  ];
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
 
-  const display = storage.map((item) => {
-    return (
-      <Card
-        cartItems={cartItems}
-        setCartItems={setCartItems}
-        itemName={item.itemName}
-        key={item.id}
-      />
-    );
-  });
-
-  return <div className={styles.shopPage}>{display}</div>;
+  return (
+    <div className={styles.shopPage}>
+      {storeURL.map((item) => {
+        return (
+          <Card
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+            itemName={item.title}
+            key={item.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default ShopPage;
+
+// const storage = [
+//   { itemName: 'Alpha', id: uniqid() },
+//   { itemName: 'Bravo', id: uniqid() },
+//   { itemName: 'Charlie', id: uniqid() },
+//   { itemName: 'Delta', id: uniqid() },
+// ];
+
+// return (
+//   <div className={styles.shopPage}>
+//     {storage.map((item) => {
+//       return (
+//         <Card
+//         cartItems={cartItems}
+//         setCartItems={setCartItems}
+//         itemName={item.itemName}
+//         key={item.id}
+//         />
+//         );
+//       })}
+//   </div>
+// );
